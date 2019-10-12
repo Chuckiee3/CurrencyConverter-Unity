ca0325878eeaa676f994cf58a189072983f3d0db
Cagri Benli <chuckseagull33@gmail.com>
2019-10-12 15:14:05 +0300

    Created base project.
    Added listeners for button taps and input fields.

---
+++ Assets/Scripts/ActionHandler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TMPro;
+using UnityEngine;
+
+public class ActionHandler : MonoBehaviour
+{
+    [SerializeField]
+    private CurrencyConverter converter;
+
+
+    [SerializeField]
+    private TMP_InputField inputDate;
+    
+    private int dateCharacterCount;
+
+    private int previousCharacterCount;
+
+    public int minimumYear = 1999;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnDateTextChanged()
+    {
+        #region InputAddRemoveMinusSign
+        
+        //Add remove - to input Correctly
+        var length = inputDate.text.Length;
+        if(previousCharacterCount < length){
+            if (length != 4 && length != 7)
+            {
+                previousCharacterCount = length;
+                return;
+            }
+            inputDate.text += "-";
+            previousCharacterCount = length + 1;
+            inputDate.caretPosition = length+1;
+        
+        }
+        else
+        {
+            if (length != 4 && length != 7) return;
+            length--;
+            previousCharacterCount = length;
+            inputDate.text = inputDate.text.Substring(0, length);
+        }
+        
+        #endregion
+
+        #region YearInputCorrection
+        
+        var index = inputDate.text.IndexOf("-", StringComparison.Ordinal);
+        if (length < 4 || index == -1 || index < 4) return;
+        var year = int.Parse(inputDate.text.Substring(0,index));
+        if (year <= minimumYear)
+        {
+            year = minimumYear;
+            inputDate.text = year.ToString() + inputDate.text.Substring(4, inputDate.text.Length - 4);
+            inputDate.caretPosition = length+1;
+            return;
+        }
+
+        if (year <= System.DateTime.Today.Year) return;
+        year = System.DateTime.Today.Year;
+        inputDate.text = year.ToString() + inputDate.text.Substring(4, inputDate.text.Length - 4);
+        inputDate.caretPosition = length+1;
+        #endregion
+
+        #region MonthInputCorrection
+
+        
+
+        #endregion
+        #region DayInputCorrection
+
+        
+
+        #endregion
+        
+        
+    }
+
+    public void OnGetCurrenciesTapped()
+    {
+    }
+
+    public void OnCalculateTapped()
+    {
+    }
+}
---
+++ Assets/Scripts/CurrencyConverter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CurrencyConverter : MonoBehaviour
+{
+    public void CheckInput(string dateStr)
+    {
+        
+    }
+
+    public void SendRequest(int year, int month, int day)
+    {
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            Debug.Log("No internet connection.");
+            return;
+        }
+        var builder = new StringBuilder();
+        var builderUri = new StringBuilder();
+ 
+        builder.Append(year);
+        builder.Append('-');
+        builder.Append(month);
+        builder.Append('-');
+        builder.Append(day);
+
+
+        var date = builder.ToString();
+        builderUri.Append("https://api.exchangeratesapi.io/");
+        builderUri.Append(date);
+        StartCoroutine(routine: GetRequest(builderUri.ToString()));
+
+    }
+
+    private static IEnumerator GetRequest(string apiUri)
+    {
+        using (var webRequest = UnityWebRequest.Get(apiUri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Network error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Received: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
