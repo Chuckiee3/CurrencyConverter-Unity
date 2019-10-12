22ca7307fbb54e4e4d61463c3d069e54c622cdf8
Cagri Benli <chuckseagull33@gmail.com>
2019-10-12 21:22:59 +0300

    Cleared code.
    Removed empty method.
    Removed unused using statement.
    Updated project settings.

--- Assets/Scripts/CurrencyConverter.cs
+++ Assets/Scripts/CurrencyConverter.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Data;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -16,10 +15,6 @@ public class CurrencyConverter : MonoBehaviour
     private void Start()
     {
         currencyListRequestActive = false;
-    }
-
-    public void CheckInput()
-    {
     }
 
     public bool SendCurrencyListRequest(string inputStr)
